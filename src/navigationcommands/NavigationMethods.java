434516963bf3a9c3ab2ffc2b1530f8fad59544ce
jayasreetpt <126882957+jayasreetpt@users.noreply.github.com>
2023-03-03 18:57:35 +0530

    A

--- src/navigationcommands/NavigationMethods.java
+++ src/navigationcommands/NavigationMethods.java
@@ -2,11 +2,13 @@ package navigationcommands;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
 public class NavigationMethods {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void navigation() {
   }
   @BeforeTest
   public void beforeTest() {
